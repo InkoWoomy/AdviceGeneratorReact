9b1b3265c2959652304d753c1ded020dbcc0b61c
Alexander <yoda4115@gmail.com>
2025-02-28 10:24:26 -0800

    =

--- src/components/page.jsx
+++ src/components/page.jsx
@@ -22,12 +22,12 @@ function Page() {
           <h2 className='text-white md:text-4xl font-bold py-10 text-2xl text-center sm:flex lg:block'>"{output.advice}"</h2>  
         </div>
         <div className='flex justify-center pb-10'>
-          <img src="/public/pattern-divider-desktop.svg" alt="Divider Desktop" className='size-auto md:hidden block'/>
-          <img src="/public/pattern-divider-mobile.svg" alt="Divider Mobile" className='size-auto md:block hidden'/>
+          <img src="public/pattern-divider-desktop.svg" alt="Divider Desktop" className='size-auto md:hidden block'/>
+          <img src="public/pattern-divider-mobile.svg" alt="Divider Mobile" className='size-auto md:block hidden'/>
         </div>
         <div className='flex justify-center'>
           <button type='button' className='bg-emerald-400 hover:shadow-2xl hover:shadow-emerald-400 focus:ring-2 focus:ring-emerald-600 rounded-full p-6 flex absolute' onClick={() => fetchAdvice()}>
-            <img src="/public/icon-dice.svg" alt="Dice" className='size-8'/>
+            <img src="public/icon-dice.svg" alt="Dice" className='size-8'/>
           </button>
         </div>
       </div>
